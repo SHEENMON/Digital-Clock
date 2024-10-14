19c99ce38b08db1ea7302f708df64a910f899ea8
SHEENMON <sheenmon.2002@gmail.com>
2024-10-14 23:13:38 +0530

    Add files via upload

---
+++ jsproject04.js
@@ -0,0 +1,30 @@
+const hours = document.getElementById("hours");
+const minutes = document.getElementById("minutes");
+const seconds = document.getElementById("seconds");
+const ampm = document.getElementById("ampm");
+
+function updateclock() {
+	let h = new Date().getHours();
+	let m = new Date().getMinutes();
+	let s = new Date().getSeconds();
+	let ampm = "AM";
+
+	if (h>12) {
+		h = h-12;
+		ampm = "PM";
+	}
+
+	h = h<10? "0" + h : h;
+	m = m<10? "0" + m : m;
+	s = s<10? "0" + s : s;
+
+	hours.innerHTML = h;
+	minutes.innerHTML = m;
+	seconds.innerHTML = s;
+	ampm.innerHTML = ampm;
+	setTimeout(()=>{
+		updateclock()
+	},1000);
+}
+
+updateclock();
